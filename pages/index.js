3763f82bb454598b03bee3c202dd4bc8e96656f8
ZhouTian0716 <egglord0716@gmail.com>
2022-09-20 20:31:08 +1000

    beginning of SSR

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
       <Head>
         <title>Pokemon List</title>
       </Head>
-      <h2>Pokemon List</h2>
+      <h2>Pokemon List SSR</h2>
       <div className={styles.grid}>
         {pokemon.map((pokemon) => (
           <div className={styles.card} key={pokemon.id}>
